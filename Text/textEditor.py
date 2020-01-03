fbae984ada48b250054f3a05317c8c7072d6e136
Zach <archibzp@outlook.com>
2020-01-03 13:10:20 -0700

    Renamed pi and e files. Updated text Editor

--- Text/textEditor.py
+++ Text/textEditor.py
@@ -4,21 +4,35 @@ Purpose: Text editor application.
 Author: Zach Archibald
 """
 
-from tkinter import *
+import tkinter as tk
+import tkinter.filedialog as tkf
+import os
 
 
-class TextEditorApplication:
-    def __init__(self, master):
+class TextEditorApplication(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
         self.master = master
         master.title("Zach's Text Editor")
+        self.pack()
+        self.create_widgets()
 
-        menu = Menu(master)
-        master.config(menu=menu)
+    def create_widgets(self):
+        menu = tk.Menu(self.master)
+        self.master.config(menu=menu)
 
-        file_menu = Menu(menu)
-        menu.add_cascade(label="File", menu=file_menu)
+        file_menu = tk.Menu(menu)
         file_menu.add_command(label="New File", command=self.new_file)
         file_menu.add_command(label="Open File", command=self.open_file)
+        file_menu.add_command(label="Save File", command=self.save_file)
+        menu.add_cascade(label="File", menu=file_menu)
+
+        scroll = tk.Scrollbar(self.master)
+        scroll.pack(side=tk.RIGHT, fill=tk.BOTH)
+        text_area = tk.Text(self.master, padx=5, pady=5, borderwidth=3)
+        text_area.pack(side=tk.LEFT, expand=1, fill=tk.BOTH)
+        text_area.config(yscrollcommand=scroll.set)
+
 
     def new_file(self):
         print("New File")
@@ -26,12 +40,14 @@ class TextEditorApplication:
     def open_file(self):
         print("Open File")
 
+    def save_file(self):
+        print("Save File")
+        save_file = tkf.asksaveasfile(mode="w", defaultextension=".txt")
+        contents = str(self.text_area.get("1.0", tk.END))
+        save_file.write(contents)
+        save_file.close()
 
-def main():
-    root = Tk()
-    editor = TextEditorApplication(root)
-    root.mainloop()
 
-
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+editor = TextEditorApplication(master=root)
+editor.mainloop()
